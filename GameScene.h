f2a87c19db8c0d7715378c0080656b50712ff7bc
田島 寛之 <tashima@000150-m2.local>
2014-11-27 18:01:12 +0900

    add GameScene.cpp & h

---
+++ GameScene.h
@@ -0,0 +1,14 @@
+#ifndef __GAMESCENE_H__
+#define __GAMESCENE_H__
+
+#include "cocos2d.h"
+
+class GameScene : public cocos2d::CCLayer
+{
+public:
+    virtual bool init();
+    static cocos2d::CCScene* scene();
+    CREATE_FUNC(GameScene);
+};
+
+#endif
